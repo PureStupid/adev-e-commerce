e0378a3b9e93bcc03986ba19aecdc9b9d5625745
PureStupid <enleteo2004@gmail.com>
2024-09-29 00:45:09 +0800

    Update .gitignore and db-connection.js

--- backend/data-access/db-connection.js
+++ backend/data-access/db-connection.js
@@ -1,12 +1,14 @@
 const mysql = require("mysql2");
+const dotenv = require("dotenv");
+dotenv.config();
 
 // Create the connection to the database
 const connection = mysql.createConnection({
-  host: "localhost",
-  port: "3306",
-  user: "root",
-  password: "adev",
-  database: "e-commerce",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 // Test DB Connection
